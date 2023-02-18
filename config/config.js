09680b9bd9436cd9272e33d698e777d8c2b160c1
brujie <54068022+brujie@users.noreply.github.com>
2023-02-18 14:26:03 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,28 +1,28 @@
 module.exports = {
   baseUrl: 'http://localhost:5000',
-  bossId: 'rujie121411', // 主人id
-  roomId: '20827419294@chatroom', // 唯一通知id
+  bossId: 'rujie666888', // 主人id
+  roomId: 'xxxx@chatroom', // 唯一通知id
   oneArtList:{
-    gh_2192f9ff1614: "唯一艺术官方",
-    gh_d8545f360464: "疯狂食客",
-    gh_e9c057778fd2: "达文奇的蛋",
-    gh_0617d1ff55f0: "丰子恺",
-    yuanjihua_art: "元计划",
-    gh_c54884439cd4: "即世科技",
-    gh_259f3c390d8c: "拓梦次元",
-    gh_a098fc69262e: "数据大师行情"
+    gh_2192f9ff1614: "xxxxx",
+    gh_d8545f360464: "xxxxx",
+    gh_e9c057778fd2: "xxxxx",
+    gh_0617d1ff55f0: "xxxxx",
+    yuanjihua_art: "xxxxx",
+    gh_c54884439cd4: "xxxxx",
+    gh_259f3c390d8c: "xxxxx",
+    gh_a098fc69262e: "xxxxx"
   },
   collection: [ 
     {
-      wxid: '48127661877@chatroom',
-      id1: '25984981889654614@openim',
+      wxid: 'xxxxx@chatroom',
+      id1: 'xxxxx@openim',
     },
     {
-      wxid: '23420164924@chatroom',
-      id1: 'wxid_2foh1as5x1p22',
+      wxid: 'xxxxx@chatroom',
+      id1: 'xxxxx',
     }
   ],
   sendList: [
-    '20670709432@chatroom'
+    'xxxxx@chatroom'
   ]
 }
